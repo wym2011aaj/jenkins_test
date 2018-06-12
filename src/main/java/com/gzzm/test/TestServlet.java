e4ebbe144b3fb27566df1708d2743ac09ecbc991
wym <1129090093@qq.com>
2018-06-12 21:32:52 +0800

    servlet test

---
+++ src/main/java/com/gzzm/test/TestServlet.java
@@ -0,0 +1,28 @@
+package com.gzzm.test;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Neo
+ * @date 2018/6/12 21:26
+ */
+public class TestServlet extends HttpServlet {
+    public TestServlet() {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.getWriter().write("hello doGet");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.getWriter().write("hello doPost");
+    }
+}
